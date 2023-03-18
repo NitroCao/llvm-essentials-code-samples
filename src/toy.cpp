6f5ce9a5b35675d0f1942abb7463cbea3f0a3d03
Nitro Cao <jaycecao520@gmail.com>
2023-03-18 22:21:02 +0800

    feat: emit a function in a module.

--- src/toy.cpp
+++ src/toy.cpp
@@ -1,12 +1,25 @@
-#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <vector>
 
 using namespace llvm;
 
 static LLVMContext context;
 static Module *moduleObj = new Module("foo", context);
 
+Function *createFunc(IRBuilder<> &builder, const std::string& name) {
+    FunctionType *functionType = FunctionType::get(builder.getInt32Ty(), false);
+    return Function::Create(functionType, Function::ExternalLinkage, name, moduleObj);
+}
+
 int main(int argc, char **argv) {
+    static IRBuilder<> builder(context);
+
+    Function *fooFunc = createFunc(builder, "foo");
+    verifyFunction(*fooFunc);
+
     moduleObj->print(errs(), nullptr);
     return 0;
 }
